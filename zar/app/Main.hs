22d33a3dc0cb49f09cdc33c8e104149359a2992c
Joe Badaczewski <joe307bad@gmail.com>
2019-11-12 10:06:24 -0500

    simple haskell example using optpase-applicative

--- zar/app/Main.hs
+++ zar/app/Main.hs
@@ -1,6 +1,16 @@
 module Main where
 
 import Lib
+import Options.Applicative
 
 main :: IO ()
-main = someFunc
+main = greet =<< execParser opts
+  where
+    opts = info (sample <**> helper)
+      ( fullDesc
+     <> progDesc "Print a greeting for TARGET"
+     <> header "hello - a test for optparse-applicative" )
+
+greet :: Sample -> IO ()
+greet (Sample h False n) = putStrLn $ "Hello, " ++ h ++ replicate n '!'
+greet _ = return ()
--- zar/src/Lib.hs
+++ zar/src/Lib.hs
@@ -1,6 +1,26 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Options.Applicative
+import Data.Semigroup ((<>))
+
+data Sample = Sample
+  { hello      :: String
+  , quiet      :: Bool
+  , enthusiasm :: Int }
+
+sample :: Parser Sample
+sample = Sample
+      <$> strOption
+          ( long "hello"
+         <> metavar "TARGET"
+         <> help "Target for the greeting" )
+      <*> switch
+          ( long "quiet"
+         <> short 'q'
+         <> help "Whether to be quiet" )
+      <*> option auto
+          ( long "enthusiasm"
+         <> help "How enthusiastically to greet"
+         <> showDefault
+         <> value 1
+         <> metavar "INT" )
