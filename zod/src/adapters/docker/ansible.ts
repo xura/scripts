a2a5dec77e943f5c44e9cb79183156ee2c5d6f29
Joe Badaczewski <joe307bad@gmail.com>
2019-12-02 23:06:23 -0500

    foundation for ping/retry functionality

--- zod/src/adapters/docker/ansible.ts
+++ zod/src/adapters/docker/ansible.ts
@@ -20,17 +20,17 @@ export default class implements Docker {
 
     async createSpaContainer(name: string): Promise<[boolean, string]> {
 
-        const containerName = `${name.replace(/\./g, '')}.${this._project}.staging.xura.io`;
+        const containerName = `${name.replace(/\./g, '')}.${this._project}.staging.xura.io`
         const stagingHtdocs = `${this._config.get('STAGING_HTDOCS')}/${this._project}/${name}`
-        const indexHtmlCdnUrl = `https://cdn.xura.io/staging/${this._project}/${name}/index.html`;
+        const indexHtmlCdnUrl = `https://cdn.xura.io/staging/${this._project}/${name}/index.html`
 
         const ansibleExtraVars = JSON.stringify({
             containerName,
             stagingHtdocs,
             indexHtmlCdnUrl,
             network: this._config.get('STAGING_DOCKER_NETWORK')
-        });
-        const command = `create-staging-url.yml -i xura --extra-vars '${ansibleExtraVars}'`;
+        })
+        const command = `create-staging-url.yml -i xura --extra-vars '${ansibleExtraVars}'`
 
         await this._playbook.command(command);
 
--- zod/src/services/deploy.ts
+++ zod/src/services/deploy.ts
@@ -3,6 +3,8 @@ import { autoInjectable, inject } from 'tsyringe'
 import { Cdn } from '../interfaces/cdn'
 import { Environment } from '../interfaces/config'
 import { Docker } from '../interfaces/docker';
+import { retry } from 'async';
+import { promise } from 'ping';
 
 export const DEPLOY_ERRORS = {
   PROPERTY_NOT_INJECTED: (property: string) => (`${property} adapter not injected properly`),
@@ -34,14 +36,31 @@ export default class {
     //      [X] upload single index.html file (path/github link passed in through args)
     //      [X] set correct subdomain and lets-encrypt domain
     //      [X] set correct docker network
-    //      [ ] ensure certs are being created, do we need DNS integration?
+    //      [ ] ensure certs are being created, do we need DNS integration? --> this seems to work, ran into a rate limit with lets encrypt
     // [ ] ping staging URL until it is reachable
+    //      [ ] create ping service
     // [ ] develop command to remove spa container:
     //      [ ] Remove staging htdocs for version
-    //      [ ] Remove container isntance
+    //      [ ] Remove container instance
     //      [ ] Remove certs from nginx/certs folder
     // [ ] output new staging url to console
 
-    return this._docker.createSpaContainer(tag);
+    //return this._docker.createSpaContainer(tag);
+    retry({
+      times: 10,
+      interval: (retryCount) => {
+        console.log(retryCount)
+        return 10
+      }
+    }, async () => {
+      if (!(await promise.probe('3f3f3rf.com')).alive) {
+        throw Error('Ping failed');
+      }
+      return true;
+    }, function (err, result) {
+      // do something with the result
+    });
+
+    return Promise.reject([false, `${tag} could not be resolved`])
   }
 }
